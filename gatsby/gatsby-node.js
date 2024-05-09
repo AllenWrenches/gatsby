db8d018e46e1183c31f3e92e8670685f7be362df
Matt Allen <mallen@iqsolutions.com>
2024-05-09 16:28:16 -0400

    everything

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,157 @@
+import path from 'path';
+import fetch from 'isomorphic-fetch';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // 3. Loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // What is the URL for this new page?
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+async function turnToppingsIntoPages({ graphql, actions }) {
+  console.log(`Turning the toppings into pages!!!`);
+  // 1. Get the template
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. query all the toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. create page for that topping
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        // TODO: Regex for topping
+      },
+    });
+  });
+  // 4. Pass topping data to pizza.js
+}
+
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // 1. Fetch a list of beers
+  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const beers = await res.json();
+  console.log(beers);
+  // 2. Loop over each one
+  for (const beer of beers) {
+    // create a node for each beer
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    // 3. Create a node for each beer
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+async function turnSliceMastersIntoPages({ graphql, actions }) {
+  // 1. Query all slicemasters
+  const { data } = await graphql(`
+    query {
+      sliceMasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // 2. Turn each slicemaster into their own page
+  data.sliceMasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: path.resolve('./src/templates/Slicemaster.js'),
+      path: `slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
+  // 3. Figure out how many pages there are based on how many slicemasters there are and how many per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.sliceMasters.totalCount / pageSize);
+  console.log(
+    `There are ${data.sliceMasters.totalCount} total people. And we have ${pageCount} pages with ${pageSize} per page`
+  );
+  // 4. Loop from 1 to n and create pages for them
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log(`Creating page ${i}`);
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      // This data is passed to the template when we create it
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of beers and source them into our Gatsby API!
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
+}
+
+export async function createPages(params) {
+  // Create pages dynamically
+  // Wait for all promises to be resolved before finishing the function
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+    turnSliceMastersIntoPages(params),
+  ]);
+  // 1. Pizzas
+  // 2. Toppings
+  // 3. Slidemasters
+}
---
+++ gatsby/src/utils/usePizza.js
@@ -0,0 +1,81 @@
+import { useContext, useState } from 'react';
+import OrderContext from '../components/OrderContext';
+import formatMoney from './formatMoney';
+import calculateOrderTotal from './calculateOrderTotal';
+import attachNamesAndPrices from './attachNamesAndPrices';
+
+export default function usePizza({ pizzas, values }) {
+  // 1. Create some state to hold our order
+  // Got rid of this line because we moved useState up to the provider
+  // const [order, setOrder] = useState([]);
+  // Now we can access both our useState and updater function (setOrder) via context
+  const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+
+  // 2. Make a function to add things to order
+  function addToOrder(orderedPizza) {
+    setOrder([...order, orderedPizza]);
+  }
+  // 3. Make a function to remove things from order
+  function removeFromOrder(index) {
+    setOrder([
+      // everything before the item we want to remove
+      ...order.slice(0, index),
+      // everything after the item we want to remove
+      ...order.slice(index + 1),
+    ]);
+  }
+
+  // This is the function that is run when someone submits the form
+  async function submitOrder(e) {
+    e.preventDefault();
+    console.log(e);
+    setLoading(true);
+    setError(null);
+    // setMessage('Go eat!');
+
+    // Gather all the data
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+      mapleSyrup: values.mapleSyrup,
+    };
+    console.log(body);
+    // TODO:4. Send this data to a serverless function when they check out
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = JSON.parse(await res.text());
+
+    // Check if everything worked
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false); // turn off loading
+      setError(text.message);
+    } else {
+      // it worked!
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza!');
+    }
+  }
+
+  return {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
+  };
+}
